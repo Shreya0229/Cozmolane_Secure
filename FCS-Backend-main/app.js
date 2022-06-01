879c5e4aead91bf63921dccfb85bf3358fe4cc46
Shreya0229 <62853109+Shreya0229@users.noreply.github.com>
2022-06-01 14:31:27 +0200

    Updated app.js

--- FCS-Backend-main/app.js
+++ FCS-Backend-main/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express'); 
 const mongoose = require('mongoose');
 const app = express();
 const rateLimit = require("express-rate-limit");
